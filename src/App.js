8ce5c8254eecbca55c3aa2ecbdafc226c1377ac1
SHEF <Shefqetmush@gmail.com>
2023-02-03 21:07:04 -0500

    validates on mouse click if character found

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div className="App">
-      <Frontpage />
+      <Frontpage locations={locations}/>
     </div>
   );
 }
--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -2,10 +2,22 @@ import React from 'react'
 import image from "../waldoimage.jpg";
 
 
-function Frontpage() {
+function Frontpage(props) {
+  // const [won,setWon] = React.useState(false)
+  const [searchList, setSearchList] = React.useState([
+    "Waldo","Odlaw","Wizard"
+  ])
+  console.log(searchList)
+
+  let currentMouseClick 
+
   function getEvent(event) {
+    if (event.target["localName"] === "button") {
+      return checkTarget(currentMouseClick, event.target.innerText);
+    } 
     let x = event.nativeEvent["offsetX"] - 40;
     let y = event.nativeEvent["offsetY"] - 40;
+    currentMouseClick = [x,y]
     setTarget(x, y);
   }
   
@@ -14,23 +26,82 @@ function Frontpage() {
     target.style.left = `${x}px`
     target.style.top = `${y}px`
   }
+
+  function checkTarget(target,name){
+    if (name === "Waldo"){
+      const [waldoX, waldoY] = [
+        props.locations[0].waldo[0],
+        props.locations[0].waldo[1],
+      ];
+      const [targetX,targetY] = [target[0], target[1]]
+      if (targetX + 25 > waldoX && targetX -25 < waldoX && targetY + 50 > waldoY && targetY - 50 < waldoY){
+        setSearchList(prevstate => {
+          let arr = [...prevstate]
+          arr = arr.filter(character => character !== name)
+          return arr
+        })
+      }
+    } else if (name === "Wizard"){
+      const [wizardX, wizardY] = [
+        props.locations[0].wizard[0],
+        props.locations[0].wizard[1],
+      ];
+      const [targetX, targetY] = [target[0], target[1]];
+      if (
+        targetX + 25 > wizardX &&
+        targetX - 25 < wizardX &&
+        targetY + 25 > wizardY &&
+        targetY - 25 < wizardY
+      ) {
+          setSearchList((prevstate) => {
+            let arr = [...prevstate];
+            arr = arr.filter((character) => character !== name);
+            return arr;
+          });
+      }
+    } else if (name === "Odlaw"){
+      const [odlawX, odlawY] = [
+        props.locations[0].odlaw[0],
+        props.locations[0].odlaw[1],
+      ];
+      const [targetX, targetY] = [target[0], target[1]];
+      if (
+        targetX + 25 > odlawX &&
+        targetX - 25 < odlawX &&
+        targetY + 25 > odlawY &&
+        targetY - 25 < odlawY
+      ) {
+         setSearchList((prevstate) => {
+           let arr = [...prevstate];
+           arr = arr.filter((character) => character !== name);
+           return arr;
+         });
+      }
+    }
+  }
+
+  let namesList = searchList.map(name => {
+    return  <li>{name}</li>
+  })
   
   return (
     <div className="main--container">
       <h1>Where's:</h1>
       <ul>
-        <li>Waldo</li>
-        <li>Odlaw</li>
-        <li>Wizard</li>
+        {namesList}
       </ul>
       <div className="image--container">
-        <div className='cursortargetcontainer'>
+        <div className="cursortargetcontainer">
           <div className="cursortarget"></div>
-          <button>Waldo</button>
-          <button>Odlaw</button>
-          <button>Wizard</button>
+          <button onClick={(event) => getEvent(event)}>Waldo</button>
+          <button onClick={(event) => getEvent(event)}>Odlaw</button>
+          <button onClick={(event) => getEvent(event)}>Wizard</button>
         </div>
-        <img onClick={(event) => getEvent(event)} src={image} alt="where's waldo"></img>
+        <img
+          onClick={(event) => getEvent(event)}
+          src={image}
+          alt="where's waldo"
+        ></img>
       </div>
     </div>
   );
